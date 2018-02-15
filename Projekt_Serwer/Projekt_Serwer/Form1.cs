8fc267fa96fd2613d447b920e955d22c1e221681
domit <domit@LAPTOP-BFLDNRBF>
2018-02-15 20:26:07 +0100

    lala

--- Projekt_Serwer/Projekt_Serwer/Form1.cs
+++ Projekt_Serwer/Projekt_Serwer/Form1.cs
@@ -10,11 +10,18 @@ using System.Windows.Forms;
 
 namespace Projekt_Serwer
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string hubileszcz;
+            hubileszcz = "dupa";
         }
     }
 }
